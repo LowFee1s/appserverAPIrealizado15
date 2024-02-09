c9c0ee2b00224c8abbd269aa11348dab7d52ae55
LowFee1s <140272488+LowFee1s@users.noreply.github.com>
2024-02-08 23:52:20 -0600

    Update server.py
    Signed-off-by: LowFee1s <140272488+LowFee1s@users.noreply.github.com>

--- server.py
+++ server.py
@@ -1,10 +1,27 @@
 import os
-from flask import Flask, send_from_directory, render_template, redirect
+from flask import Flask, request, send_from_directory, render_template, redirect
 
 app = Flask(__name__)
 
 port = int(os.environ.get("PORT", 5000))
 
+usuario_localizaciones = {}
+
+@app.route('/actualizar_ubicacion', methods=['POST', 'OPTIONS'])
+def actualizar_ubicacion():
+    if request.method == 'POST':
+        data = request.get_json()
+        id_usuario = data['id_usuario']
+        localizacion = data['localizacion']
+        usuario_localizaciones['id_usuario] = localizacion
+        return "Ubicacion Actualizada"
+        elif request.method == 'OPTIONS':
+            return ("", 200)
+
+@app.route('/obtener_ubicacion', methods=['GET'])
+def obtener_ubicacion():
+        return usuario_localizaciones
+        
 @app.route('/static/<path:path>')
 def serve_static(path):
     return send_from_directory('static', path)
